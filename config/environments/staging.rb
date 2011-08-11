a0bf2b6934f93f54e2dd75b362a03f8465965942
Len Smith <len@barrison.com>
2011-08-10 20:43:52 -0400

    added staging environment file

---
+++ config/environments/staging.rb
@@ -0,0 +1,34 @@
+# Settings specified here will take precedence over those in config/environment.rb
+
+# In the development environment your application's code is reloaded on
+# every request.  This slows down response time but is perfect for development
+# since you don't have to restart the webserver when you make code changes.
+config.cache_classes = false
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+
+# Raise error message if the mailer cannot send
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
+
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.sendmail_settings = {
+  :location       => '/usr/sbin/sendmail',
+  :arguments      => '-i -t'
+}
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "spring.joyent.us",
+  :port                 => 25,
+  :domain               => "spring.joyent.us",
+  :user_name            => "info-linguazone",
+  :password             => "tamina01",
+  :authentication       => :plain
+}
