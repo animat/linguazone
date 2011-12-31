2673f0b83052789b921ee88180519370f5205cf5
Colin Angevine <info@linguazone.com>
2011-12-30 19:35:27 -0500

    Various small changes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -120,6 +120,7 @@ class StudentsController < ApplicationController
     respond_to do |format|
       if User.is_email_in_use(@user.email) 
         flash[:error] = "That username or email address is already in the database."
+        format.html { render :action => "new" }
       else
         if @user.save
           format.html { redirect_to :controller => "students", :action => "index" }
