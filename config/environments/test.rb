954e03b17a736c43dbda644d4d9b56256345cf35
Len Smith <len@barrison.com>
2012-02-22 16:48:50 -0500

    Removed deprecation warnings form rspec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,8 +1,9 @@
 Linguazone::Application.configure do
-  config.cache_classes = false
-  config.whiny_nils = true
+  config.cache_classes                                 = false
+  config.whiny_nils                                    = true
   config.action_controller.perform_caching             = false
   config.action_view.cache_template_loading            = true
   config.action_controller.allow_forgery_protection    = false
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method                 = :test
+  config.active_support.deprecation                    = :stderr
 end
