fac6e7cd84ea06d0836f2cb45839c608ca3d3193
Colin Angevine <info@linguazone.com>
2012-08-15 10:43:56 -0400

    Added alternate route for RSS feeds (corrected)

--- config/routes.rb
+++ config/routes.rb
@@ -84,7 +84,7 @@ Linguazone::Application.routes.draw do
   match 'customize/:action/:cmzr_type/:id' => 'customize#index', :cmzr_type => nil, :id => nil
   match 'class/:id' => 'courses#show'
   match 'class/:id/feed' => 'courses#feed'
-  match 'course/:id/feed' => 'courses#feed'
+  match 'courses/:id/feed' => 'courses#feed'
     
   # TODO: Is there any way to simplify these statements?
   match 'about/pricing' => 'about#pricing', :as => :pricing
