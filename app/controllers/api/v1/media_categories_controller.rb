c0f728a10ed2935c8dc45353175f8997d4391d3d
Colin Angevine <info@linguazone.com>
2012-08-28 16:41:14 -0400

    Still trying to correct image and audio bugs in customizer

--- app/controllers/api/v1/media_categories_controller.rb
+++ app/controllers/api/v1/media_categories_controller.rb
@@ -2,7 +2,8 @@ class  Api::V1::MediaCategoriesController < ApplicationController
   respond_to :xml
   
   def index
-    @media_categories = MediaCategory.active
+    #@media_categories = MediaCategory.active #TODO: This query does not work on postgres
+    @media_categories = MediaCategory.all
     # TODO @Len: Isn't there a better way to force an XML response on the index?
     render :template => "api/v1/media_categories/index.xml.builder", :layout => false
   end
