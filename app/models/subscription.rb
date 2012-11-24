6affc07ba4b17cfbc379aa3322ba7263f1bebc6b
Colin Angevine <colinangevine@gmail.com>
2012-11-23 23:01:46 -0500

    Fixing syntax error in Subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -39,37 +39,37 @@ class Subscription < ActiveRecord::Base
     unless @two_week_subs.empty?
       InvoiceMailer.two_week_reminder(@two_week_subs).deliver
     end
-    @notified_users.push {:two_week_subs => @two_week_subs}
+    @notified_users << {:two_week_subs => @two_week_subs}
     
     @one_week_subs = User.joins(:subscription, :subscription_plan).merge(Subscription.days_remaining(7)).merge(SubscriptionPlan.paid_sub)
     unless @one_week_subs.empty?
       InvoiceMailer.one_week_reminder(@one_week_subs).deliver
     end
-    @notified_users.push {:one_week_subs => @one_week_subs}
+    @notified_users << {:one_week_subs => @one_week_subs}
     
     @two_day_subs = User.joins(:subscription, :subscription_plan).merge(Subscription.days_remaining(2)).merge(SubscriptionPlan.paid_sub)
     unless @two_day_subs.empty?
       InvoiceMailer.two_day_reminder(@two_day_subs).deliver
     end
-    @notified_users.push {:two_day_subs => @two_day_subs}
+    @notified_users << {:two_day_subs => @two_day_subs}
     
     @one_week_trials = User.joins(:subscription, :subscription_plan).merge(Subscription.days_remaining(7)).merge(SubscriptionPlan.trial_sub)
     unless @one_week_trials.empty?
       InvoiceMailer.trial_one_week_reminder(@one_week_trials).deliver
     end
-    @notified_users.push {:one_week_trials => @one_week_trials}
+    @notified_users << {:one_week_trials => @one_week_trials}
     
     @three_day_trials = User.joins(:subscription, :subscription_plan).merge(Subscription.days_remaining(3)).merge(SubscriptionPlan.trial_sub)
     unless @three_day_trials.empty?
       InvoiceMailer.trial_three_day_reminder(@three_day_trials).deliver
     end
-    @notified_users.push {:three_day_trials => @three_day_trials}
+    @notified_users << {:three_day_trials => @three_day_trials}
     
     @expired_trials = User.joins(:subscription, :subscription_plan).merge(Subscription.days_remaining(-1)).merge(SubscriptionPlan.trial_sub)
     unless @expired_trials.empty?
       InvoiceMailer.trial_expired_reminder(@expired_trials).deliver
     end
-    @notified_users.push {:expired_trials => @expired_trials}
+    @notified_users << {:expired_trials => @expired_trials}
   end
   
   InvoiceMailer.expiration_admin_update(@notified_users).deliver
