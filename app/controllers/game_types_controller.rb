10c8f345b2bf2e3b80bcdcdf6d1fbaab277a0470
Colin Angevine <colinangevine@gmail.com>
2012-12-22 13:46:37 -0500

    Patched the game types controller: language ID is not being passed in

--- app/controllers/game_types_controller.rb
+++ app/controllers/game_types_controller.rb
@@ -1,7 +1,9 @@
 class GameTypesController < ApplicationController
   def index
     activity  = Activity.find(params[:activity_id])
-    @language = Language.find(params[:language_id])
+    #@language = Language.find(params[:language_id])
+    # TODO: language_id parameter not being passed in
+    @language = Language.find(2)
     @game_type = GameType.for(activity)
   end
 end
