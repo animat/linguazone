5b4ccf12dcaedaaed92a0161f9bc91f88e2c3c93
Colin Angevine <info@linguazone.com>
2014-11-23 10:32:11 -0500

    Further trying to make debugging easier on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,7 @@ Linguazone::Application.configure do
 
   
   config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
+  config.consider_all_requests_local = ENV['CONSIDER_REQUESTS_LOCAL']
   
   config.cache_classes = false
 
