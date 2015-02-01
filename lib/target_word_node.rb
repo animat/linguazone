f1529154bba82ccd0cce5d68fa7377bdc2b199c6
Len Smith <len@barrison.com>
2015-01-31 21:54:16 -0500

    Update Target Word Documentation

--- lib/target_word_node.rb
+++ lib/target_word_node.rb
@@ -51,37 +51,36 @@ class TargetWordNode < GameDataNode
   # Append more XML before writing the options node
   def before_options_xml(xml);end
 end
-__END__
-example xml:
 
-  <?xml version="1.0" encoding="utf-8"?>
+__END__
+
+= Example XML:
+
+<?xml version="1.0" encoding="utf-8"?>
 <xml>
-	<format gamename="Leap Frog" v="2.0">
-		<attribute name="english" type="question" label="English word" type="editbox" interactive="true" />
-		<attribute name="lang" type="response" label="translation" type="editbox" interactive="true" />
-	</format>
-	<description>
-		<text>Type in the Latin for the given verb forms.</text>
-		<keyword>active </keyword>
-		<keyword>passive</keyword>
-	</description>
-	
-	<gamedata>
-		<node>
-			<question name="english" content="30" type="audio" />
-	<response name="lang" content="facietur" type="text" />
-		</node>
-		
-		<node>
-			<question name="english" content="I will be loved" type="text" />
-			<response name="lang" content="amabor" type="text" />
-		</node>
-		
-		<node>
-			<question name="english" content="media/image/food/cake.swf" type="image" />
-		</node>
-	</gamedata>
-	
-	<templatedata></templatedata>
-	<language>Spanish</language>
-</xml><F5>
+  <format gamename="Garden Grows" v="2.0">
+    <attribute name="english" type="question" label="English word" type="editbox" interactive="true" />
+  </format>
+  <description>
+    <text>Type in the Latin for the given verb forms.</text>
+    <keyword>active </keyword>
+    <keyword>passive</keyword>
+  </description>
+
+  <gamedata>
+    <node>
+      <question name="english" content="30" type="audio" />
+    </node>
+
+    <node>
+      <question name="english" content="I will be loved" type="text" />
+    </node>
+
+    <node>
+      <question name="english" content="media/image/food/cake.swf" type="image" />
+    </node>
+  </gamedata>
+
+  <templatedata></templatedata>
+  <language>Spanish</language>
+</xml>
