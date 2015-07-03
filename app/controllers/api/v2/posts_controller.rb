e47ab9b9db1db07e355e10d6de4cced5189885fc
Colin Angevine <info@linguazone.com>
2015-07-03 15:39:31 -0400

    Added API v2 support for posts. Not yet authorizing properly.

---
+++ app/controllers/api/v2/posts_controller.rb
@@ -0,0 +1,25 @@
+class Api::V2::PostsController < ApplicationController
+  include Transloadit::Rails::ParamsDecoder
+  require 'aws/s3'
+  
+  # TODO: Need to enforce authorization on API!
+  #filter_access_to :show
+
+  def show
+    @ap = AvailablePost.includes({:comments => :user}).find(params[:id])
+    @post = Post.find(@ap.post_id)
+    @course = Course.find(@ap.course_id)
+    unless current_user.nil?
+      @comment = Comment.new( :available_post => @ap )
+    
+      @student_comments = @ap.comments
+      @student_comments = @student_comments.page(params[:page])
+      
+      if current_user.is_student?
+        @solo_comments = @ap.comments.where(:user_id => current_user.id)
+        @solo_comments = @solo_comments.page(params[:page])
+      end
+    end
+    render json: {post: @post, course: @course, comments: @student_comments}
+  end
+end
