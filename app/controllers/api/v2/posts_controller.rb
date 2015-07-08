54e7bd45508abbd93ae85b09717a47e1ea06d735
Colin Angevine <info@linguazone.com>
2015-07-08 09:35:13 -0400

    Simplified posts controller in API v2

--- app/controllers/api/v2/posts_controller.rb
+++ app/controllers/api/v2/posts_controller.rb
@@ -9,17 +9,6 @@ class Api::V2::PostsController < ApplicationController
     @ap = AvailablePost.includes({:comments => :user}).find(params[:id])
     @post = Post.find(@ap.post_id)
     @course = Course.find(@ap.course_id)
-    unless current_user.nil?
-      @comment = Comment.new( :available_post => @ap )
-    
-      @student_comments = @ap.comments
-      @student_comments = @student_comments.page(params[:page])
-      
-      if current_user.is_student?
-        @solo_comments = @ap.comments.where(:user_id => current_user.id)
-        @solo_comments = @solo_comments.page(params[:page])
-      end
-    end
-    render json: {post: @post, comments: @student_comments.to_json(:include => {:user => {:only => :display_name}})}
+    render json: {post: @post, comments: @ap.comments.to_json(:include => {:user => {:only => :display_name}})}
   end
 end
