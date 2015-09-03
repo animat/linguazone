e66eedb38cb26493fda329db59234e10cabf8f24
Colin Angevine <info@linguazone.com>
2015-09-02 23:26:24 -0400

    Embedding HTML5 games via iframe

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -6,6 +6,12 @@ class PlayController < ApplicationController
   end
   
   def show
+    if Rails.env.production?
+      @game_src = "http://linguazone.s3.amazonaws.com/games"
+    else
+      @game_src = "http://lz-staging.s3.amazonaws.com/games"
+    end
+    
     @ag = AvailableGame.find(params[:id])
     @game = Game.find(@ag.game_id, :include => [:activity, :updated_by])
     @premium = (@game.updated_by.nil?) ? false : @game.updated_by.is_premium_subscriber?
