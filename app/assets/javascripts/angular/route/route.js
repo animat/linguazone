e7481bce6ac59aca87e6573afc13ce4993219b5e
Ahmad Bilal <ahmad@gems.techverx.com>
2015-12-23 20:53:20 +0500

    show content on application

--- app/assets/javascripts/angular/route/route.js
+++ app/assets/javascripts/angular/route/route.js
@@ -1,10 +1,10 @@
-linguazone_customizer.config(function($stateProvider, $urlRouterProvider){
-	$urlRouterProvider.otherwise(function ($injector){
-		var $state = $injector.get('$state');
-		$state.go('home');
-	});
-	return $stateProvider.state('home', {
-		url: "/home",
-		templateUrl: "/assets/home.html"
-	});
-});
+// linguazone_customizer.config(function($stateProvider, $urlRouterProvider){
+// 	$urlRouterProvider.otherwise(function ($injector){
+// 		var $state = $injector.get('$state');
+// 		$state.go('home');
+// 	});
+// 	return $stateProvider.state('home', {
+// 		url: "/home",
+// 		templateUrl: "/assets/home.html"
+// 	});
+// });
