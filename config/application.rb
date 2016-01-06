211a9702e7cd841e5df856659a6ef74bd4e66b4d
Ahmad Bilal <ahmad@gems.techverx.com>
2016-01-06 22:09:03 +0500

    fix for pg connection error heroku

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Linguazone
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names.
     config.time_zone = 'Eastern Time (US & Canada)'
-
+    config.assets.initialize_on_precompile = false
     config.action_mailer.default_url_options = { :host => "linguazone.com" }
     
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
