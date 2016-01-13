90f2cc712ab151feaf87be5a3596cbc65958bb8b
Ahmad Bilal <ahmad@gems.techverx.com>
2016-01-13 21:15:12 +0500

    Step 2

--- app/assets/javascripts/angular/controllers/home-ctrl.js
+++ app/assets/javascripts/angular/controllers/home-ctrl.js
@@ -11,8 +11,7 @@ function HomeCtrl ($scope, $state, HomeService, x2js, $filter) {
 		$scope.heading = 'Step 1: Select a language';
 		$scope.heading_description = 'Select a language for activity.';
 		$scope.jsonObj = 	{ 
-     						xml : {
-      						}
+     						xml: {}
 						};
 		convertToXML($scope.jsonObj);
 		var promise = HomeService.getLanguages();
@@ -27,7 +26,27 @@ function HomeCtrl ($scope, $state, HomeService, x2js, $filter) {
 	};
 	$scope.init();
 
+	function camelize(str) {
+		return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+			return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+		}).replace(/\s+/g, '');
+	}
+
 	$scope.editXML = function(){
+
+		// Include name into XML
+		if (typeof($scope.activity_name) !== 'undefined' && $scope.activity_name !== ''){
+			var swfString = camelize($scope.activity_name).replace(/\s/g, '');
+			var formatString = {format : {_gamename : $scope.activity_name, _gameSWF: swfString}}; 
+			$scope.jsonObj["xml"] = formatString;
+			convertToXML($scope.jsonObj);
+		};
+
+		// Include Description into XML
+		if (typeof($scope.description_name) !== 'undefined' && $scope.description_name !== ''){
+			$scope.jsonObj["xml"]["description"] = {text : $scope.description_name};
+		};
+
 		if (typeof($scope.language) !== 'undefined' && $scope.language !== ''){
 			var language = getSpecificLanguage($scope.language);
 			$scope.jsonObj["xml"]["language"] = language.name; 
